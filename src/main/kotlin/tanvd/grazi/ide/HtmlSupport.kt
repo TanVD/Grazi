63821639ca2be81165aa3e7c220fe3a61c6005fb
AlexKaneRUS <AlexKaneRUS@gmail.com>
2019-03-26 21:40:49 +0300

    improved html support

--- src/main/kotlin/tanvd/grazi/ide/HtmlSupport.kt
+++ src/main/kotlin/tanvd/grazi/ide/HtmlSupport.kt
@@ -1,19 +1,16 @@
 package tanvd.grazi.ide
 
 
-import com.intellij.ide.highlighter.HtmlFileType
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiFileFactory
-import com.intellij.psi.impl.source.html.HtmlDocumentImpl
 import com.intellij.psi.impl.source.html.HtmlFileImpl
+import com.intellij.psi.impl.source.xml.XmlTextImpl
 import com.intellij.psi.util.PsiTreeUtil
 
 class HtmlSupport : GraziLanguageSupport {
     override fun replace(textBlock: TextBlock, range: TextRange, replacement: String) {
         val newText = range.replace(textBlock.element.text, replacement)
-        val newFile = PsiFileFactory.getInstance(textBlock.element.project).createFileFromText("a.html", HtmlFileType.INSTANCE, newText) as HtmlFileImpl
-        textBlock.element.replace(collectParagraphs(newFile).single())
+        (textBlock.element as XmlTextImpl).value = newText
     }
 
     override fun extract(file: PsiFile): List<TextBlock>? {
@@ -23,6 +20,7 @@ class HtmlSupport : GraziLanguageSupport {
         }
     }
 
-    private fun collectParagraphs(htmlFile: HtmlFileImpl): MutableCollection<HtmlDocumentImpl> =
-            PsiTreeUtil.collectElementsOfType(htmlFile, HtmlDocumentImpl::class.java)
+    private fun collectParagraphs(htmlFile: HtmlFileImpl): MutableCollection<XmlTextImpl> {
+        return PsiTreeUtil.collectElementsOfType(htmlFile, XmlTextImpl::class.java)
+    }
 }
