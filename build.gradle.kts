6b3c37f1eb0d005150dfe15256422de3e4832971
Nikita Sokolov <faucct@gmail.com>
2019-03-26 23:31:32 +0300

    Dependency slf4j should not be duplicated

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,18 +32,15 @@ detekt {
     }
 }
 
+configurations.all {
+    exclude("org.slf4j", "slf4j-api")
+}
+
 dependencies {
     compileOnly(kotlin("stdlib"))
     compileOnly(kotlin("reflect"))
     compile("org.languagetool", "languagetool-core", "4.4")
-    {
-        exclude("org.slf4j", "slf4j-api")
-    }
     compile("org.apache.commons", "commons-lang3", "3.5")
-
     compile("org.languagetool", "language-all", "4.4")
-    {
-        exclude("org.slf4j", "slf4j-api")
-    }
     testCompile("org.junit.jupiter", "junit-jupiter-api", "5.0.2")
 }
