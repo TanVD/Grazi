352f245a47b66a69e3d0a50473906af6cfb2610b
Vladislav.Tankov <vdtankov@gmail.com>
2019-08-08 01:07:15 +0300

    Style fixes and minor refactorings

--- buildSrc/src/main/kotlin/tanvd/grazi/Utils.kt
+++ buildSrc/src/main/kotlin/tanvd/grazi/Utils.kt
@@ -36,38 +36,3 @@ inline fun <reified Value> jbProperties() = System.getProperties()
         .mapKeys { it.key as String }.mapValues { it.value as Value }.toMutableMap()
 
 fun execArguments() = System.getProperty("exec.args", "").split(",")
-
-fun ExternalModuleDependency.excludesForLT() {
-    exclude("org.slf4j", "slf4j-api")
-
-    // useless for languagetool-core
-    exclude("com.typesafe.akka")
-    exclude("org.scala-lang")
-    exclude("biz.k11i", "xgboost-predictor")
-
-    // already in IDEA
-    exclude("com.google.guava", "guava")
-    exclude("com.intellij", "annotations")
-    exclude("net.java.dev.jna", "jna")
-    exclude("javax.xml.bind", "jaxb-api")
-    exclude("org.glassfish.jaxb", "jaxb-runtime")
-    exclude("com.fasterxml.jackson.core", "jackson-databind")
-
-    // used only in tests in languagetool-core
-    exclude("org.apache.lucene", "lucene-core")
-    exclude("org.apache.lucene", "lucene-backward-codecs")
-}
-
-fun DependencyHandler.addAetherDependencies() {
-    _compile ("org.eclipse.aether", "aether-connector-basic", "1.1.0")
-    _compile ("org.eclipse.aether", "aether-transport-file", "1.1.0")
-    _compile ("org.eclipse.aether", "aether-transport-http", "1.1.0") {
-        exclude("org.slf4j", "slf4j-api")
-        exclude("org.apache.httpcomponents", "httpclient")
-    }
-    _compile("org.apache.maven", "maven-aether-provider", "3.3.9"){
-        exclude("org.slf4j", "slf4j-api")
-        exclude("com.google.guava", "guava")
-        exclude("org.apache.commons", "commons-lang3")
-    }
-}
--- plugin/src/main/kotlin/tanvd/grazi/ide/init/GraziProjectInit.kt
+++ plugin/src/main/kotlin/tanvd/grazi/ide/init/GraziProjectInit.kt
@@ -3,9 +3,6 @@ package tanvd.grazi.ide.init
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
-import com.intellij.spellchecker.inspections.SpellCheckingInspection
-import com.intellij.util.Consumer
-import com.intellij.vcs.commit.message.CommitMessageInspectionProfile
 import tanvd.grazi.GraziConfig
 import tanvd.grazi.ide.msg.GraziStateLifecycle
 
