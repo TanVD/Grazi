014bad68f80404eed97f9d9a02f400342f2ffba4
Vladislav Tankov <vdtankov@gmail.com>
2019-08-18 21:18:26 +0300

    Reuse spellchecker rule from LanguageTool for Grammar from SpellChecker

--- plugin/src/main/kotlin/tanvd/grazi/ide/GraziCommitInspection.kt
+++ plugin/src/main/kotlin/tanvd/grazi/ide/GraziCommitInspection.kt
@@ -5,7 +5,6 @@ import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.codeInspection.ex.LocalInspectionToolWrapper
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElementVisitor
 import com.intellij.vcs.commit.message.BaseCommitMessageInspection
 import com.intellij.vcs.commit.message.CommitMessageInspectionProfile
 import tanvd.grazi.GraziConfig
@@ -25,9 +24,7 @@ class GraziCommitInspection : BaseCommitMessageInspection() {
 
     override fun getDefaultLevel(): HighlightDisplayLevel = HighlightDisplayLevel.find("TYPO") ?: HighlightDisplayLevel.WARNING
 
-    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
-        return graziInspection.buildVisitor(holder, isOnTheFly)
-    }
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) = graziInspection.buildVisitor(holder, isOnTheFly)
 
     override fun getDisplayName() = "Grazi proofreading inspection for VCS"
 }
